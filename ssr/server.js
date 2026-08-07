031883b70904286ee6776f101b1204075d76756e
Serhat Can <serhatcan1996@gmail.com>
2026-08-08 00:50:39 +0200

    feat: init ssr project scaffold and simple server to serve ssr

---
+++ ssr/server.js
@@ -0,0 +1,35 @@
+import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+import { renderToString } from "react-dom/server";
+import { createElement as h } from "react";
+import App from "./App.js";
+
+// Following code is just very simple version of SSR
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const distPath = path.join(__dirname, "dist");
+
+const shell = readFileSync(path.join(__dirname, "dist", "index.html"), "utf-8");
+
+const app = fastify();
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "dist"),
+  prefix: "/",
+});
+
+const parts = shell.split("<!--ROOT-->");
+app.get("/", (req, reply) =>
+{
+  // this part immediately sends the head part
+  reply.raw.write(parts[0]);
+  const reactApp = renderToString(h(App));
+  reply.raw.write(reactApp);
+  reply.raw.write(parts[1]);
+  reply.raw.end();
+})
+
+app.listen({ port: 3000 });
