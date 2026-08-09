e28f1f5ee49ddb2abf72d6515f498fa7b254f560
Serhat Can <serhatcan1996@gmail.com>
2026-08-09 23:50:03 +0200

    feat: implement useTransition example

---
+++ transitions/src/Score.jsx
@@ -0,0 +1,26 @@
+const loadingUrl = "/images/loading.webp";
+
+export default function Score({
+  isPending,
+  home,
+  homeName,
+  homeImage,
+  away,
+  awayName,
+  awayImage,
+}) {
+  return (
+    <div className="score">
+      <div>
+        <h2>{isPending ? "HOME" : homeName}</h2>
+        <h3>{isPending ? "-" : home}</h3>
+        <img src={isPending ? loadingUrl : homeImage} alt="home team logo" />
+      </div>
+      <div>
+        <h2>{isPending ? "AWAY" : awayName}</h2>
+        <h3>{isPending ? "-" : away}</h3>
+        <img src={isPending ? loadingUrl : awayImage} alt="away team logo" />
+      </div>
+    </div>
+  );
+}
