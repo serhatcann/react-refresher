17cd2ba08e8b229a8ca499d63a60e7809f9485e5
Serhat Can <serhatcan1996@gmail.com>
2026-08-10 14:53:30 +0200

    feat: implement simple useDeferredValue example

---
+++ deferred/src/DisplayImage.jsx
@@ -0,0 +1,19 @@
+import img from "../images/luna.jpg";
+import { memo } from "react";
+const JANK_DELAY = 100;
+
+export default memo(function DisplayImage({ filterStyle }) {
+  const expensiveRender = () => {
+    const start = performance.now();
+    while (performance.now() - start < JANK_DELAY) {}
+    return null;
+  };
+
+  return (
+    <>
+      {expensiveRender()}
+      <img src={img} alt="Luna" style={{ filter: filterStyle }} />
+      <p>Last render: {Date.now()}</p>
+    </>
+  );
+});
